6a5afa4e4ccef1cefbe09c90eb254bca9cded4ed
Michael Huffman <huffman442@yahoo.com>
2018-09-28 14:16:12 -0400

    Changed to make all numbers print out on the same line

--- FibonacciSequence/FibonacciSequence.py
+++ FibonacciSequence/FibonacciSequence.py
@@ -1,17 +1,19 @@
+import sys
+
 print("How many Fibonacci Numbers would you like to generate?")
 n = int(input())
 x = 1
 previous_x = 0
 for loopCount in range(1, n):
     if (loopCount == 1):
-        print("0, 1, ")
+        sys.stdout.write("0, 1, ")
     elif (loopCount == n-1):
         next_x = x + previous_x
         previous_x = x
         x = next_x    
-        print(f"{next_x}")
+        sys.stdout.write(f"{next_x}")
     else:
         next_x = x + previous_x
         previous_x = x
         x = next_x    
-        print(f"{next_x}, ")
+        sys.stdout.write(f"{next_x}, ")
