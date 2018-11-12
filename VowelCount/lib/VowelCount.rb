8fcc5a5829b5a34d0873b5d6338892ae6688f898
Michael Huffman <huffman442@yahoo.com>
2018-11-12 15:17:46 -0500

    added each vowel complexity

---
+++ VowelCount/lib/VowelCount.rb
@@ -0,0 +1,21 @@
+class VowelCount
+    def count word
+        vowels = Hash.new(0)
+        word.each_char do |char|
+            char.downcase!
+            if(char == 'a') #||char == 'e'||char == 'i'||char == 'o' || char == 'u')
+                vowels[char] += 1
+            elsif(char == 'e')
+                vowels[char] += 1
+            elsif(char == 'i')
+                vowels[char] += 1
+            elsif(char == 'o')
+                vowels[char] += 1
+            elsif(char == 'u')
+                vowels[char] += 1
+            end
+        end
+        puts vowels
+        vowels
+    end
+end
